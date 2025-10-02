90b2893672737d52e2c6cb673a5ad7ae7b211987
Jacksim1995 <70483691+Jacksim1995@users.noreply.github.com>
2025-10-02 20:36:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+
+  const picks = parseInt(document.getElementById('picks').value);
+  const runners = parseInt(document.getElementById('runners').value);
+
+  const horses = [];
+
+  if (picks > runners) {
+    alert('Picks must be lower than runners.');
+    return;
+  }
+
+  while (horses.length < picks) {
+      let number = Math.ceil(Math.random()*runners);
+      if (horses.includes(number) === false) { 
+        horses.push(number);
+      }
+    }
+
+  document.getElementById('result').innerHTML = horses.join(", ");
+})
